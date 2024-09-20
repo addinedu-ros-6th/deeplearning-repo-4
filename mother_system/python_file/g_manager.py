3029209f57e1b772f6d8b4387a2c2a175b69a2ce
ImSR-KN <lim18637@gmail.com>
2024-09-20 15:20:38 +0900

    Update g_manager.py

--- mother_system/python_file/g_manager.py
+++ mother_system/python_file/g_manager.py
@@ -24,13 +24,13 @@ class GManager:
 
     def run(self, g_pipe):
         # self.goose_tcp.start_server()
-        while True:
-            if g_pipe.poll():
-                
-                self.value = g_pipe.recv()
-                #print(f"g_manager : {self.value}")
-                cv2.imshow("gcamera frame", self.value)
-                cv2.waitKey(1)
+        #while True:
+        #    if g_pipe.poll():
+        #        
+        #        self.value = g_pipe.recv()
+        #        #print(f"g_manager : {self.value}")
+        #        cv2.imshow("gcamera frame", self.value)
+        #        cv2.waitKey(1)
                 #g_pipe.send(self.value + 1)
 
 
